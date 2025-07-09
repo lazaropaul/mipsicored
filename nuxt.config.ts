8ba6e879a98aae696c1b429b0d372cb5deb50cde
Paul <lazaro.roca.pol@gmail.com>
2025-07-09 15:51:41 +0200

    SSR set to true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+  ssr: true,
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
   },
