72da2092ae888a9e4f7f199c2c44f31261e7971d
Paul <lazaro.roca.pol@gmail.com>
2025-09-09 18:19:08 +0200

    Worker email test

--- server/api/mail_conf.post.ts
+++ server/api/mail_conf.post.ts
@@ -1,14 +1,17 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 import { randomUUID } from "crypto";
-import nodemailer from "nodemailer";
 
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event)
-  const config = useRuntimeConfig()
+  const body = await readBody(event);
+  const config = useRuntimeConfig();
+  var info = { messageId: "" };
 
-  const supabase = createClient(config.public.supabaseUrl, config.public.supabaseKey)
+  const supabase = createClient(
+    config.public.supabaseUrl,
+    config.public.supabaseKey
+  );
 
-  const token = randomUUID()
+  const token = randomUUID();
 
   const { error } = await supabase.from("verifications").insert({
     email: body.email,
@@ -16,33 +19,70 @@ export default defineEventHandler(async (event) => {
     expires_at: new Date(Date.now() + 1000 * 60 * 60), // 1h
   });
 
-  const verifyUrl = `${config.public.siteUrl}/api/verify?token=${token}&email=${body.email}`
+  const verifyUrl = `${config.public.siteUrl}/api/verify?token=${token}&email=${body.email}`;
 
-  const transporter = nodemailer.createTransport({
-    host: "mipsicored-com.correoseguro.dinaserver.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: "no-reply@mipsicored.com",
-      pass: config.noreplyPasswd,
-    },
-  });
-  
-  try {
-    const info: nodemailer.SentMessageInfo = await transporter.sendMail({
-      from: '"Departamento de soporte | Mipsicored" <no-reply@mipsicored.com>',
-      to: body.email,
-      subject: "Confirma tu cuenta en Mipsicored",
-      html: `<h1>¡Hola "${body.firstName}"!</h1><br><b>Por favor confirma tu cuenta haciendo clic en el siguiente enlace:</b><br>
-             <a href="${verifyUrl}">Confirmar cuenta</a>`,
+  //TODO: Clean up spaghettti code
+
+  // Check if running in development environment
+  if (import.meta.dev) {
+    // Development
+    const nodemailer = await import("nodemailer");
+    const transporter = nodemailer.createTransport({
+      host: "mipsicored-com.correoseguro.dinaserver.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "no-reply@mipsicored.com",
+        pass: config.noreplyPasswd,
+      },
     });
+
+    try {
+      info = await transporter.sendMail({
+        from: '"Departamento de soporte | Mipsicored" <no-reply@mipsicored.com>',
+        to: body.email,
+        subject: "Confirma tu cuenta en Mipsicored",
+        html: `<h1>¡Hola "${body.firstName}"!</h1><br><b>Por favor confirma tu cuenta haciendo clic en el siguiente enlace:</b><br>
+             <a href="${verifyUrl}">Confirmar cuenta</a>`,
+      });
+
+      return { success: true, messageId: info.messageId };
+    } catch (error) {
+      console.error("Error sending mail:", error);
+      throw createError({
+        statusCode: 500,
+        statusMessage: "Failed to send email",
+      });
+    }
+  } else {
+    // Production
+    const { WorkerMailer } = await import("worker-mailer");
+    const mailer = await WorkerMailer.connect({
+      credentials: {
+        username: "no-reply@mipsicored.com",
+        password: config.noreplyPasswd,
+      },
+      host: "mipsicored-com.correoseguro.dinaserver.com",
+      port: 465,
+      secure: true,
+    });
+    try {
+      await mailer.send({
+        from: { name: "Departamento de soporte | Mipsicored", email: "no-reply@mipsicored.com" },
+        to: { name: "Registro", email: body.email  },
+        subject: "Confirma tu cuenta en Mipsicored",
+        html: `<h1>¡Hola "${body.firstName}"!</h1><br><b>Por favor confirma tu cuenta haciendo clic en el siguiente enlace:</b><br>
+             <a href="${verifyUrl}">Confirmar cuenta</a>`,
+      });
+    } catch (error) {
+      console.error("Error sending mail:", error);
+      throw createError({
+        statusCode: 500,
+        statusMessage: "Failed to send email",
+      });
+    }
 
     return { success: true, messageId: info.messageId };
-  } catch (error) {
-    console.error("Error sending mail:", error);
-    throw createError({
-      statusCode: 500,
-      statusMessage: "Failed to send email",
-    });
+
   }
 });
